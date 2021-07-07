8f8453855f1d2adb2f6659b5cd40c321a12a0a83
Franklin <forsonfranklin@gmail.com>
2021-07-07 18:40:11 +0000

    initial commit

---
+++ src/test/java/com/afroprogrammer/spring/boot/soap/api/SpringBootSoapWsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afroprogrammer.spring.boot.soap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapWsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
