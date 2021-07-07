29be9bb6129f8af86a27be18c67b048ce367aaf8
Franklin <forsonfranklin@gmail.com>
2021-07-07 18:39:40 +0000

    initial commit

---
+++ src/main/java/com/afroprogrammer/spring/boot/soap/api/SpringBootSoapWsApiApplication.java
@@ -0,0 +1,13 @@
+package com.afroprogrammer.spring.boot.soap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapWsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSoapWsApiApplication.class, args);
+    }
+
+}
